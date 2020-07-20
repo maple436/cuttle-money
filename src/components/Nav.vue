8b662bb0e059aaefab219c0253782f7466f9a0e2
maple <2313694690@qq.com>
2020-07-20 19:57:12 +0800

    创建<Icon/>组件

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,21 +1,24 @@
 <template>
   <div class="nav">
+    <Icon name="label"/>
     <router-link to="/labels">标签</router-link>
     |
+    <Icon name="money"/>
     <router-link to="/money">记账</router-link>
     |
+    <Icon name="statistics"/>
     <router-link to="/statistics">统计</router-link>
   </div>
 </template>
 
 <script lang="ts">
+
+
   export default {
     name: 'Nav'
   };
 </script>
 
 <style lang="scss" scoped>
-  .nav{
-    border:1px solid red;
-  }
+
 </style>
