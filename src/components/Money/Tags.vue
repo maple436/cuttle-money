36b4c835ed15a73cade7b617dac2e46057073788
maple <2313694690@qq.com>
2020-07-22 21:18:15 +0800

    解决tags 数据类型问题

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -4,8 +4,9 @@
       <button @click="create">新增标签</button>
     </div>
     <ul class="current">
-      <li v-for="tag in dataSource" :key="tag"  :class="{selected:selectedTags.indexOf(tag)>=0}"
-      @click="toggle(tag)">
+      <li v-for="tag in dataSource" :key="tag"
+          :class="{selected: selectedTags.indexOf(tag)>=0}"
+          @click="toggle(tag)">
         {{tag}}
       </li>
     </ul>
@@ -15,33 +16,33 @@
 
 <script lang="ts">
   import Vue from 'vue';
-  import {Component,Prop} from 'vue-property-decorator';
+  import {Component, Prop} from 'vue-property-decorator';
 
   @Component
-  export default class Tags extends Vue{
-    @Prop(String) dataSource: string[] | undefined;
-    selectedTags: string[]=[];
+  export default class Tags extends Vue {
+    @Prop() dataSource: string[] | undefined;
+    selectedTags: string[] = [];
 
-    toggle(tag: string){
-      const index=this.selectedTags.indexOf(tag);
-      if(index>=0){
-        this.selectedTags.splice(index,1)
+    toggle(tag: string) {
+      const index = this.selectedTags.indexOf(tag);
+      if (index >= 0) {
+        this.selectedTags.splice(index, 1);
 
-      }else{
+      } else {
         this.selectedTags.push(tag);
       }
-      this.$emit('update:value',this.selectedTags)
+      this.$emit('update:value', this.selectedTags);
     }
 
-    create(){
-      const name=window.prompt('请输入标签名');
-      if(name===''){
+    create() {
+      const name = window.prompt('请输入标签名');
+      if (name === '') {
         window.alert('标签名不能为空');
-      }else if(this.dataSource){
-          this.$emit('update:dataSource',
-            [...this.dataSource,name])
-        }
+      } else if (this.dataSource) {
+        this.$emit('update:dataSource',
+          [...this.dataSource, name]);
       }
+    }
   }
 </script>
 
@@ -52,11 +53,13 @@
     flex-grow: 1;
     display: flex;
     flex-direction: column-reverse;
+
     > .current {
       display: flex;
       flex-wrap: wrap;
+
       > li {
-        $bg:#d9d9d9;
+        $bg: #d9d9d9;
         background: $bg;
         $h: 24px;
         height: $h;
@@ -65,14 +68,17 @@
         padding: 0 16px;
         margin-right: 12px;
         margin-top: 4px;
-        &.selected{
-          background: darken($bg,50%);
-          color:white;
+
+        &.selected {
+          background: darken($bg, 50%);
+          color: white;
         }
       }
     }
+
     > .new {
       padding-top: 16px;
+
       button {
         background: transparent;
         border: none;
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -31,36 +31,36 @@
   })
   export default class Money extends Vue {
     tags = ['衣', '食', '住', '行'];
-    recordList: Record[]=recordList;
+    recordList: Record[] = recordList;
     record: Record = {
-      tags:[], notes: '', type: '-', amount: 0
+      tags: [], notes: '', type: '-', amount: 0
     };
 
     onUpdateTags(value: string[]) {
-      this.record.tags=value;
+      this.record.tags = value;
     }
 
     onUpdateNotes(value: string) {
-      this.record.notes=value;
+      this.record.notes = value;
     }
 
     onUpdateAmount(value: string) {
-      this.record.amount=parseFloat(value);
+      this.record.amount = parseFloat(value);
     }
 
-    onUpdateType(value: string){
-      this.record.type=value;
+    onUpdateType(value: string) {
+      this.record.type = value;
     }
 
-    saveRecord(){
-      const record2: Record=JSON.parse(JSON.stringify(this.record))
-      record2.createdAt=new Date();
+    saveRecord() {
+      const record2: Record = JSON.parse(JSON.stringify(this.record));
+      record2.createdAt = new Date();
       this.recordList.push(record2);
     }
 
     @Watch('recordList')
-    onRecordListChang(){
-      window.localStorage.setItem('recordList',JSON.stringify(this.recordList));
+    onRecordListChang() {
+      window.localStorage.setItem('recordList', JSON.stringify(this.recordList));
     }
 
   }
